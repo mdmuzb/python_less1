aeac43337ab0a0bf14167c5ef6c4e43d70ded8fc
mdm <navruzbek@gmail.com>
2022-08-07 21:53:37 +0500

    задача 1

---
+++ exer1.py
@@ -0,0 +1,7 @@
+
+car_model =input("Введите модель вашего авто=")
+car_color = input("укажите цвет авто=")
+car_year = input("год производство авто=")
+print(f"Модель авто - {car_model},"
+   f"Цвет авто - {car_color}")
+print("Год выпуска: " +car_year)
