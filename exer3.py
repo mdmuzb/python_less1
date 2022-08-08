47eaab9fad98dd0444d8ad0f6622d4aa226cd1cd
mdm <navruzbek@gmail.com>
2022-08-08 23:35:01 +0500

    задача №3

---
+++ exer3.py
@@ -0,0 +1,9 @@
+
+n = input("Введите число:")
+summa = int(n)+int(str(n)+str(n))+int(str(n)+str(n)+str(n))
+print(f"Сумма чисел = {summa}")
+
+n = input("Введите число:")
+res =int(n)+int(n*2)+int(n*3)
+
+print(f"result={res}")
